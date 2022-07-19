fec9182bdf6ceac3eafc2349c33a74e392686d3f
Daniel Reis <daniel.wur@googlemail.com>
2022-07-19 13:46:05 +0000

    test 2

---
+++ README.md
@@ -0,0 +1,5 @@
+## Deploying a NodeJS app to heroku from Github.
+
+### Heroku-Link:
+
+https://reis-database.herokuapp.com/
---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+// Auth0
+const { auth } = require("express-openid-connect");
+// Cors package so that we can fetch data from frontend site
+const cors = require("cors");
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.AUTH_SECRET,
+  baseURL: process.env.AUTH_BASEURL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASEURL,
+};
+
+// Middlewares
+app.use(cors());
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// Import dotenv-config
+require("dotenv/config");
+
+app.use(bodyParser.json());
+
+// Import Routes
+const usersRoute = require("./routes/users");
+const sessionsRoute = require("./routes/sessions");
+
+// Middlewares
+app.use("/users", usersRoute);
+app.use("/sessions", sessionsRoute);
+
+// Routes
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+// .get() shoots back a message
+app.get("/", (req, res) => {
+  res.send("We are live on heroku!");
+});
+
+// Connect to DB (mongodb)
+mongoose.connect(process.env.DB_CONNECTION, () => {
+  console.log("Connected to DB!");
+});
+
+// How to start listening to the server -> port 3000
+app.listen(process.env.PORT || 3000, () => {
+  console.log("You are connected!");
+});
